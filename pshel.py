6b4c750f7c46d7627489a369ed9b42d8cecd1da5
miles <miles>
2026-05-03 12:52:05 -0500

    slight update, but this project is no longer active as I've lost interest

--- pshel.py
+++ pshel.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-# TODO: make list of already existing linux commands
+# Grabs list of existing commands on open
+# TODO: Maybe make this write to a settings file so it doesn't have to check each time the shell opens
 existing_cmds = os.listdir("/bin/")
 
 # remade cd command
@@ -23,13 +24,14 @@ def dir_nav(area):
 # TODO: make loop to take input
 while True:
 
-    line = input("#> ")
+    curdir = os.getcwd()
+    line = input(curdir + "#> ")
     line_tokens = line.split(" ")
 
     if line_tokens[0] in existing_cmds:
         os.system(line)
     elif line_tokens[0] == 'cd':
         dir_nav(line_tokens[1])
-    
-    
+    elif line_tokens[0] == 'exit':
+        quit()
 
